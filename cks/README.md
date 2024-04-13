91304d414120effccb408aa65ec8df388fb2c64d
gio <giordano2407@gmail.com>
2024-04-13 16:11:20 +0200

    tf fmt + add ssh config to readme.md

--- cks/README.md
+++ cks/README.md
@@ -3,3 +3,13 @@ This sub-directory contains helpful tips, tricks, and commands to assist in prep
 
 # Node Provisioning
 Use the Terraform code in the *aws* folder to provision two EC2 instances that can serve as a master-worker pair for a sandbox Kubernetes cluster.
+
+## SSH access
+```bash
+# configuration for EC2 instances; Ubuntu (!) machines
+Host *amazonaws.com
+  User ubuntu
+  IdentityFile ~/.ssh/cks-machines.pem
+  IdentitiesOnly yes
+  CheckHostIP no
+```
--- cks/aws/main.tf
+++ cks/aws/main.tf
@@ -22,13 +22,13 @@ module "vpc" {
 module "ec2_instances" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.6.1" # Specify the version that suits your needs
-  
+
   for_each = toset(["master", "worker"])
-  name = "instance-${each.key}"
+  name     = "instance-${each.key}"
 
   ami                    = var.ami_id # Replace this with a valid AMI for your region
   instance_type          = "t3.medium"
-  key_name               = "aws-cks-machines"           # The key created on AWS in EC2 service section; chmod 400 ~/.ssh/aws-cks-machines.pem
+  key_name               = "cks-machines"               # The key created on AWS in EC2 service section; chmod 400 ~/.ssh/<KEY>.pem
   subnet_id              = module.vpc.public_subnets[0] # Using the first public subnet
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
